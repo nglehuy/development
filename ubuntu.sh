fb33523c815f58d5e8ec71b7dcacee17b3455a48
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-09-22 13:11:45 +0700

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
 
+sudo snap install go --classic
+
+sudo snap install node --classic
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+
 sudo apt install -y gcc clang ruby git make cmake openssh-client wget curl fzf \
     p7zip unrar unzip zip bzip2 gzip lrzip lz4 lzip lzop xz-utils tar \
     openssl zlibc readline-common sqlite llvm tk libffi7 libffi-dev \
     vim neovim zsh \
     default-jdk default-jre maven \
     ffmpeg libboost-all-dev swig sox \
-    neofetch htop xdotool xclip ctags
-
-sudo snap install go
-
-sudo snap install nodejs --classic
+    neofetch htop xdotool xclip ctags \
+    yarn
