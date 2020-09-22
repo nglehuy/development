0baa425e594d19e2d4bdc00a7765ed4dae2a3ceb
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-09-22 14:04:27 +0700

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,7 +8,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
 
-sudo apt install -y gcc clang ruby git make cmake openssh-client wget curl fzf \
+sudo apt install -y gcc g++ clang ruby git make cmake openssh-client wget curl fzf \
     p7zip unrar unzip zip bzip2 gzip lrzip lz4 lzip lzop xz-utils tar \
     openssl zlibc readline-common sqlite llvm tk libffi7 libffi-dev \
     vim neovim zsh \
