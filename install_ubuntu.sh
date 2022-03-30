992580d97cdd56e3ded5ce7c38233a74a4cb71cc
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2022-03-30 19:08:32 +0700

    update

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
 
-sudo snap install go --classic
+wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
 
 sudo apt update
 
-sudo apt install -y gcc g++ clang ruby git make cmake openssh-client wget curl openvpn \
+sudo apt install -y gcc g++ clang ruby git make cmake openssh-client wget curl fzf \
     p7zip unrar unzip zip bzip2 gzip lrzip lz4 lzip lzop xz-utils tar \
-    openssl readline-common sqlite llvm tk libffi-dev \
+    openssl zlibc readline-common sqlite llvm tk libffi7 libffi-dev \
     vim neovim zsh \
     default-jdk default-jre maven \
     ffmpeg libboost-all-dev swig sox \
-    neofetch htop xdotool xclip universal-ctags \
-    docker docker-compose
+    neofetch htop xdotool xclip ctags \
+    ruby-dev
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
