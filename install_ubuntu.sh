42652d4bcce2c321fe4637e9c8c336202f3281d0
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2022-06-04 12:31:58 +0700

    fix: update install ubuntu

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
 
 sudo apt update
 
+wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+sudo dpkg -i nvim-linux64.deb
+
 sudo apt install -y gcc g++ clang ruby git make cmake openssh-client wget curl fzf \
     p7zip unrar unzip zip bzip2 gzip lrzip lz4 lzip lzop xz-utils tar \
     openssl zlibc readline-common sqlite llvm tk libffi7 libffi-dev \
-    vim neovim zsh \
+    vim zsh \
     default-jdk default-jre maven \
     ffmpeg libboost-all-dev swig sox \
     neofetch htop xdotool xclip ctags \
