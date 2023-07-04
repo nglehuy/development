9844b5f5fffb99ee05dd879d470a59fc601c42ce
nglehuy <nlhuy.cs.16@gmail.com>
2023-07-05 01:35:43 +0700

    fix: update

--- install_macos.sh
+++ install_macos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-brew install ffmpeg neofetch swig sox vim neovim git python3 espeak rust go make cmake gcc openssl curl wget fzf clang-format gradle maven openjdk
+brew install ffmpeg neofetch swig sox vim neovim git python3 espeak rust go make cmake gcc openssl curl wget fzf clang-format gradle maven openjdk llvm libomp
 
 sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
