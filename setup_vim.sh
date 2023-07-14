e4cede8d0d8e8ccf21e255ebd8c13be99bf72ea4
nglehuy <nlhuy.cs.16@gmail.com>
2023-07-14 13:29:11 +0700

    chore: update

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,6 +6,9 @@ rm -rf $HOME/.vim/plugged
 echo "Install python neovim ..."
 python3 -m pip install --user neovim
 
+echo "Install nvim as editor for git ..."
+git config --global core.editor "nvim"
+
 echo "Setting up configurations ..."
 cp $PWD/home/.vimrc $HOME/.vimrc
 mkdir -p $HOME/.config/nvim && yes | cp -r $PWD/config/nvim/* $HOME/.config/nvim
