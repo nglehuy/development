6ae5890ddeea4ddc4b2250b5bcb29b449e330b3d
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2024-09-26 20:25:11 +0700

    fix: update

--- install_macos.sh
+++ install_macos.sh
@@ -3,7 +3,7 @@
 brew install \
     ffmpeg neofetch swig sox vim neovim git git-lfs python3 python@3.10 python@3.11 python@3.12 espeak \
     rust go make cmake gcc jemalloc openssl curl wget fzf \
-    clang-format gradle maven openjdk llvm libomp mecab automake grpc protobuf \
+    clang-format gradle maven openjdk openjdk@11 llvm libomp mecab automake grpc protobuf \
     bazelisk coreutils \
     openssh openvpn postgresql@14 kubectl \
     starship google-cloud-sdk \
@@ -14,3 +14,4 @@ brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 
 sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
