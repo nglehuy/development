1606073908a4e1dbfc97c1aca4aeeb7ad17a4a93
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2025-07-16 00:11:57 +0700

    chore: patch microsoft autoupdater

---
+++ patch_microsoft_macos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo rm -rf /Library/Application\ Support/Microsoft/MAU2.0
+sudo rm -rf /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper
+sudo rm -rf /Library/LaunchAgents/com.microsoft.autoupdate.helper.plist
+sudo rm -rf /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist
